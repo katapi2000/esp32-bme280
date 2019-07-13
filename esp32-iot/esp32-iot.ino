b75e494217fb7cfb97ba34ea5e6d91065019af4f
katapi20 <katapi2000@gmail.com>
2019-07-13 20:46:05 +0900

    #2 ambientに送信出来るようにした

--- esp32-iot/esp32-iot.ino
+++ esp32-iot/esp32-iot.ino
@@ -2,22 +2,30 @@
 #include <Adafruit_BME280.h>
 #include <SPI.h>
 #include <WiFi.h>
+#include <Ambient.h>
 #include "time.h"
 #include "config.h"
 
+WiFiClient client;
+Ambient ambient;
 
 //wifiのパスワードとか
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWD;
+
+//ambient
+unsigned int channelId = CHANNELID;
+const char* writeKey = WRITE_KEY;
 
 //NTPserver
 const char* ntpServer = "ntp.jst.mfeed.ad.jp";    //日本のNTPサーバー
 const long  gmtOffset_sec = 9 * 3600;   //時差9h
 const int   daylightOffset_sec = 0;   //サマータイムなし
 
+//GPIOピンアサイン master = ESP32, slave = BME280
 #define BME_CSB 26
-#define BME_SDI 13
-#define BME_SDO 12
+#define BME_SDI 13  // master -> output, slave -> input
+#define BME_SDO 12  // master -> input, slave -> output
 #define BME_SCK 14
 
 //SPI通信を使う(高速だし)
@@ -36,23 +44,36 @@ void setup() {
         while (1);
     }
 
+    //wifi確認用LED
+    pinMode(2, OUTPUT);
+    digitalWrite(2, LOW);
+    
     wifi();
 
     //init and get the time
     configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+    //ambient送信初期化
+    // |チャンネルID | ライトキー | WiFiClientの管理データ |
+    ambient.begin(channelId, writeKey, &client);
 }
 
 void loop() { 
+    //wifi接続したら点灯
+    digitalWrite(2, HIGH);
+    
     //wifi再接続
     while(WiFi.status() != WL_CONNECTED) {
-      digitalWrite(2,LOW);
+      digitalWrite(2,LOW);  //切断したら消灯
       Serial.println("Reconnecting...");
       delay(1000);
       wifi();
     }
     printLocalTime();
     printValues();
-    delay(3000);
+    ambientSend();
+    delay(500000);
+    Serial.println();
 }
 
 
@@ -68,8 +89,6 @@ void printValues() {
     Serial.print("Pressure = ");
     Serial.print(bme.readPressure() / 100.0F);
     Serial.println(" hPa");
-
-    Serial.println();
 }
 
 void printLocalTime()
@@ -93,5 +112,14 @@ void wifi() {
 
   Serial.print("WiFI connected\r IP adress: ");
   Serial.println(WiFi.localIP());
+}
+
+void ambientSend() {
+  ambient.set(1, bme.readTemperature());  //1,温度
+  ambient.set(2, bme.readHumidity()); //2,湿度
+  ambient.set(3, bme.readPressure() / 100.0F);  //3,気圧
+
+  ambient.send(); //送信
+  Serial.println("sent data");
 }
 
