451c3eb1398c2c56e92422eb91822c2444ac20eb
katapi20 <katapi2000@gmail.com>
2019-08-08 01:22:25 +0900

    途中経過

--- esp32-iot/esp32-iot.ino
+++ esp32-iot/esp32-iot.ino
@@ -1,29 +1,17 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <WiFiClientSecure.h>
 #include <SPI.h>
-#include <WiFi.h>
-#include <Ambient.h>
-#include <ESPmDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
-#include <time.h>
 #include "config.h"
-
-WiFiClient client;
-Ambient ambient;
+WiFiClientSecure client;
 
 //wifiのパスワードとか
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWD;
 
-//ambient
-unsigned int channelId = CHANNELID;
-const char* writeKey = WRITE_KEY;
-
-//NTPserver
-const char* ntpServer = "ntp.jst.mfeed.ad.jp";    //日本のNTPサーバー
-const long  gmtOffset_sec = 9 * 3600;   //時差9h
-const int   daylightOffset_sec = 0;   //サマータイムなし
+//google script
+const char* server = "script.google.com";
+const char* key = SCRIPT_ID;
 
 //GPIOピンアサイン master = ESP32, slave = BME280
 #define BME_CSB 26
@@ -34,162 +22,63 @@ const int   daylightOffset_sec = 0;   //サマータイムなし
 //SPI通信を使う(高速だし)
 Adafruit_BME280 bme(BME_CSB, BME_SDI, BME_SDO, BME_SCK); // software SPI
 
-//スリープ時間の定義
-#define uS_TO_S_FACTOR 1000000  //マイクロ秒から秒に変換
-#define TIME_TO_SLEEP  5 //スリープ時間(秒)
-
-//RTCメモリにスリープ回数を保存
-RTC_DATA_ATTR int bootCount = 0;
-
 void setup() {
-    Serial.begin(115200);
-    WiFi.mode(WIFI_STA);
-    ++bootCount;  //ブート回数を増やす
-    Serial.println("Boot number: " + String(bootCount));  //ブート回数の表示
+  Serial.begin(115200);
 
-    bool status;
-    
-    // default settings
-    // (you can also pass in a Wire library object like &Wire2)
-    status = bme.begin(0x76);  
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring!");
-        while (1);
-    }
-
-    //####################
-    //ArduinoOTAは削除しない
-    //####################
-    ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-
-    ArduinoOTA.begin();
-    //####################
-
-    //wifi確認用LED
-    pinMode(2, OUTPUT);
-    digitalWrite(2, LOW);
-    
-    wifi();
-
-    //時間の取得
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-
-    //ambient送信初期化
-    // |チャンネルID | ライトキー | WiFiClientの管理データ |
-    ambient.begin(channelId, writeKey, &client);
-
-    //### main ###
-    main_func();
-
-    //ウェイクアップソースの定義
-    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-
-    //スリープ実行
-    Serial.println("sleep now");
-    delay(1000);
-    Serial.flush();
-    esp_deep_sleep_start();
+  //BME280が接続されていないとき
+  bool status;
+  status = bme.begin(0x76);
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
 }
 
 void loop() {
-  //なにも書かない
-}
-
-void main_func() { 
-    //####################
-    //削除しない
-    ArduinoOTA.handle();
-    //####################
-    
-    //wifi接続したら点灯
-    digitalWrite(2, HIGH);
-    
-    //wifi再接続
-    while(WiFi.status() != WL_CONNECTED) {
-      digitalWrite(2,LOW);  //切断したら消灯
-      Serial.println("Reconnecting...");
-      delay(1000);
-      wifi();
-    }
-    printLocalTime();
-    printValues();
-    ambientSend();
-
-    Serial.print("sleep");
-    //ウェイクアップソースの定義
-    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-    //スリープ
-    esp_deep_sleep_start();
-}
-
-
-void printValues() {
-    Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" ℃");
-
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
-
-    Serial.print("Pressure = ");
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
-}
-
-void printLocalTime()
-{
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "%Y-%m-%d %H:%M:%S");
+  delay(10000); //10秒毎に書き込みする
+  wifi();
+  send_to_google();
+  WiFi.disconnect();
 }
 
 void wifi() {
-  Serial.printf("Connecting to %s", ssid);
-  Serial.println();
-  WiFi.mode(WIFI_STA);  //wifi子機として動作
-  WiFi.begin(ssid, password); //wifi apに接続
-  while(WiFi.status() != WL_CONNECTED) {  //wifi ap待機
+  WiFiServer server(80);
+  // Wi-Fiに接続
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    // 接続するまで待機する
     delay(1000);
-    Serial.println("wait...");
   }
-
-  Serial.print("WiFI connected\r IP adress: ");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  server.begin();
 }
 
-void ambientSend() {
-  ambient.set(1, bme.readTemperature());  //1,温度
-  ambient.set(2, bme.readHumidity()); //2,湿度
-  ambient.set(3, bme.readPressure() / 100.0F);  //3,気圧
+void send_to_google() {
+  String URL = "https://script.google.com/macros/s/";
+  URL += key;
+  URL += "/exec?";
+  URL += "&1_cell=";
+  URL += sensor_data1 ;
+  URL += "&2_cell=";
+  URL += sensor_data2 ;
+  URL += "&3_cell=";
+  URL += sensor_data3 ;
 
-  ambient.send(); //送信
-  Serial.println("sent data");
+  Serial.println(URL);
+  // サイトにアクセス
+  Serial.println("\nStarting connection to server...");
+  if (!client.connect(server, 443))
+    Serial.println("Connection failed!");
+  else {
+    Serial.println("Connected to server!\n");
+    client.println("GET " + URL);
+    client.stop();
+    Serial.println("finish.");
+  }
 }
