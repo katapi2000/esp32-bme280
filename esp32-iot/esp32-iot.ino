4d0436ab5e2f8703e5bddbff8bc5783f762a6fac
katapi20 <katapi2000@gmail.com>
2019-08-08 01:40:23 +0900

    不要なものを削除、ambient使わない

--- esp32-iot/esp32-iot.ino
+++ esp32-iot/esp32-iot.ino
@@ -24,6 +24,7 @@ Adafruit_BME280 bme(BME_CSB, BME_SDI, BME_SDO, BME_SCK); // software SPI
 
 void setup() {
   Serial.begin(115200);
+  wifi();
 
   //BME280が接続されていないとき
   bool status;
@@ -35,16 +36,22 @@ void setup() {
 }
 
 void loop() {
-  delay(10000); //10秒毎に書き込みする
-  wifi();
+
+  //wifi再接続
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Reconnecting...");
+    wifi();
+    delay(1000);
+  }
+  printValues();
   send_to_google();
-  WiFi.disconnect();
+  delay(10000); //10秒毎に書き込みする
 }
 
 void wifi() {
   WiFiServer server(80);
   // Wi-Fiに接続
-  Serial.print("Attempting to connect to SSID: ");
+  Serial.println("Attempting to connect to SSID: ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -59,16 +66,30 @@ void wifi() {
   server.begin();
 }
 
+void printValues() {
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" ℃");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+}
+
 void send_to_google() {
   String URL = "https://script.google.com/macros/s/";
   URL += key;
   URL += "/exec?";
   URL += "&1_cell=";
-  URL += sensor_data1 ;
+  URL += bme.readTemperature() ;
   URL += "&2_cell=";
-  URL += sensor_data2 ;
+  URL += bme.readHumidity() ;
   URL += "&3_cell=";
-  URL += sensor_data3 ;
+  URL += (bme.readPressure() / 100.0F) ;
 
   Serial.println(URL);
   // サイトにアクセス
